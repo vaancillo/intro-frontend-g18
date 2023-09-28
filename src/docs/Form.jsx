063aacf1472c5c4421919124f574ae6b9a3a2204
vancillo <yaredbane@gmail.com>
2023-09-27 23:34:11 -0600

    useDebounce custom hook

--- src/docs/Form.jsx
+++ src/docs/Form.jsx
@@ -1,32 +1,59 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import useDebounce from '../hooks/useDebounce'
 
 function Form () {
   const [value, setValue] = useState('')
-  const [users, getUsers] = useState({})
+  const [users, setUsers] = useState([])
+
+  const debouncedValue = useDebounce(value, 1000)
 
   const searchUsers = async () => {
-    const response = await axios.get(`https://dummyjson.com/users/search?q=${value}&limit=10&select=firstName,image`)
-    console.log(response)
-    // if (response && response.data) {
-
-    // }
+    const response = await axios.get(`https://dummyjson.com/users/search?q=${value}&limit=10&select=firstName,image,lastName`)
+    if (response && response.data) {
+      setUsers(response?.data?.users || [])
+    }
   }
-
-  useEffect(() => {
-    searchUsers()
-  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
     setValue(e.target.value)
   }
+
+  // useEffect(() => {
+  //   // searchUsers()
+  //   console.log(value)
+  // }, [value])
+
+  useEffect(() => {
+    searchUsers()
+    console.log(debouncedValue)
+  }, [debouncedValue])
+
   return (
-    <section className='container py-5'>
-      <h2 className='text-white'>{value}</h2>
+    <section className='container py-5 text-white'>
       <form>
         <input onChange={handleSubmit} className='form-control' type='search' placeholder='search...' />
       </form>
+      <div className='row py-4'>
+        {
+          users.length === 0 ? <p>No hay usuarios</p> : <p>Usuarios encontrados</p>
+        }
+        {
+          users.map(({ id, image, firstName, lastName }) => {
+            return (
+              <article key={id} className='col-md-4'>
+                <div className='card bg-dark'>
+                  <img src={image} alt={firstName} className='img-fluid' />
+                  <div className='card-body'>
+                    <h5 className='card-title'>{firstName} {lastName}</h5>
+                  </div>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
     </section>
   )
 }
---
+++ src/hooks/useDebounce.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+function useDebounce (value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => clearTimeout(timer)
+  }, [value])
+  return debouncedValue
+}
+
+export default useDebounce
